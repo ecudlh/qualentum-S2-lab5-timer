c5677be8173799330345dfc7e7bd9b45f5967fd8
Elena Cuevas de la Hera <elena.cuevas@vasscompany.com>
2025-10-17 19:55:19 +0200

    add timer part component

---
+++ library/timer/TimerPartComponent.js
@@ -0,0 +1,53 @@
+import { LitElement, html, css } from 'https://unpkg.com/lit@latest?module';
+
+export class TimerPartComponent extends LitElement {
+    static properties = {
+        value: { type: Number, attribute: true },
+        label: { type: String, attribute: true }
+    }
+    
+    constructor() {
+        super();
+        this.value = 0;
+        this.label = '';
+    }
+
+    static styles = css `
+        :host {
+            font-family: sans-serif;
+            text-align: center;
+        }
+
+        .value {
+            background: #334eff;
+            color: white;
+            font-size: 1.8rem;
+            font-weight: bold;
+            padding: 0.8rem 1.2rem;
+            border-radius: 0.5rem;
+        }
+
+        .label {
+            font-size: 0.8rem;
+            color: #1c1c1c;
+            margin-top: 0.25rem;
+            text-transform: uppercase;
+            letter-spacing: 0.05em;
+        }
+    `;
+
+    formatValue() {
+        return this.value.toString().padStart(2, '0');
+    }
+
+    render() {
+        return html `
+            <div>
+                <div class="value">${this.formatValue()}</div>
+                <div class="label">${this.label}</div>
+            </div>
+        `;
+    }
+}
+
+window.customElements.define('timer-part-component', TimerPartComponent)
